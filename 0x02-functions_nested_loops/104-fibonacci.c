c8c35519102834efc099755534d4ce780d5b1732
Smartkhid <saddatiddrisu123@gmail.com>
2023-03-22 11:25:03 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - prints the sum of all even Fibonacci numbers below 4,000,000
+ * main - prints the Fibonacci numbers that start with 1 and 2
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
+    int i, num1 = 1, num2 = 2, next;
 
+    printf("%d, %d, ", num1, num2);
+
+    for (i = 2; i < 98; i++) {
+        next = num1 + num2;
+        printf("%d", next);
+
+        if (i < 97) {
+            printf(", ");
+        }
+
+        num1 = num2;
+        num2 = next;
+    }
+
+    printf("\n");
+    return 0;
 }
