db85a82b673e2450e9d8970a873207087d4ec682
Smartkhid <saddatiddrisu123@gmail.com>
2023-03-22 10:31:10 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *void print_alphabet: a function
+ *
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
+
