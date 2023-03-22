7a9ab29a42b4272e02ca4cd0795bbcfce20466ee
Smartkhid <saddatiddrisu123@gmail.com>
2023-03-22 11:39:48 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,22 +8,23 @@
 
 int main(void)
 {
-    int i, num1 = 1, num2 = 2, next;
+	unsigned long long i, num1 = 1, num2 = 2, next;
 
-    printf("%d, %d, ", num1, num2);
+    	printf("%d, %d, ", num1, num2);
 
-    for (i = 2; i < 98; i++) {
-        next = num1 + num2;
-        printf("%d", next);
+    	for (i = 2; i < 98; i++) 
+	{
+        	next = num1 + num2;
+        	printf("%d", next);
 
-        if (i < 97) {
-            printf(", ");
+        	if (i < 97) {
+            	printf(", ");
         }
 
-        num1 = num2;
-        num2 = next;
-    }
+        	num1 = num2;
+        	num2 = next;
+    	}
 
-    printf("\n");
-    return 0;
+    	printf("\n");
+    	return 0;
 }
