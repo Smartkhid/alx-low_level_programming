e37de0b679153467249167769402a774cc745e47
Smartkhid <saddatiddrisu123@gmail.com>
2023-04-06 15:05:12 -0700

    done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,14 +7,14 @@
 int wildcmp(char* s1, char* s2) {
     int i = 0;
 
-    while (string1[i] != '\0' && string2[i] != '\0') {
-        if (string1[i] != string2[i]) {
+    while (s1[i] != '\0' && s2[i] != '\0') {
+        if (s1[i] != s2[i]) {
             return 0;  // strings are not identical
         }
         i++;
     }
 
-    if (string1[i] == '\0' && string2[i] == '\0') {
+    if (s1[i] == '\0' && s2[i] == '\0') {
         return 1;  // strings are identical
     }
     else {
