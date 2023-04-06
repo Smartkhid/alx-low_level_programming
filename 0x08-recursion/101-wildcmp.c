bb409e2a5ac182bc5a0021a26d14fc98adfd878e
Smartkhid <saddatiddrisu123@gmail.com>
2023-04-06 15:02:49 -0700

    done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,1 +1,23 @@
 #include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: the string to count
+ * Return: length of the string
+ */
+int wildcmp(char* s1, char* s2) {
+    int i = 0;
+
+    while (string1[i] != '\0' && string2[i] != '\0') {
+        if (string1[i] != string2[i]) {
+            return 0;  // strings are not identical
+        }
+        i++;
+    }
+
+    if (string1[i] == '\0' && string2[i] == '\0') {
+        return 1;  // strings are identical
+    }
+    else {
+        return 0;  // strings are not identical
+    }
+}
